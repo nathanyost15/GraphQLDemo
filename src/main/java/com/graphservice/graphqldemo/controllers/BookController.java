f3d5e5d9c01bc5fe120ec038f4a758fb1098e8a0
nathanyost15 <nathanyost15@gmail.com>
2024-11-26 21:17:49 -0500

    Add filter for books graphql to search by name

--- src/main/java/com/graphservice/graphqldemo/controllers/BookController.java
+++ src/main/java/com/graphservice/graphqldemo/controllers/BookController.java
@@ -1,6 +1,7 @@
 package com.graphservice.graphqldemo.controllers;
 
 import com.graphservice.graphqldemo.models.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -11,10 +12,13 @@ import java.util.UUID;
 @Controller
 public class BookController {
     @QueryMapping
-    public Iterable<Book> books() {
+    public Book books(@Argument String name) {
         List<Book> books = new ArrayList<>();
         books.add(new Book("Emperors New Groove", UUID.randomUUID().toString()));
         books.add(new Book("Narnia", UUID.randomUUID().toString()));
-        return books;
+        return books.stream()
+                .filter(book -> name.equalsIgnoreCase(book.getName()))
+                .findFirst()
+                .orElse(null);
     }
 }
