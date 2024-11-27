aa07e8a49a5a403924d32034b5da33d1c3cecc78
nathanyost15 <nathanyost15@gmail.com>
2024-11-26 19:34:30 -0500

    init commit

---
+++ src/test/java/com/graphservice/graphqldemo/GraphqldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphservice.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
